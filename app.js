ff2676eb83b3f00aa04f463a0964405c68070135
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-09-04 16:52:04 +0200

    Add visual aid in case of empty name fields

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const signUpForm = document.getElementById('signup-form');
 const submitButton = document.getElementById('signup-btn');
 
-
 signUpForm.addEventListener('submit', (e) => comparePasswords(e));
+submitButton.addEventListener('click', () => checkMissingNames());
 
 function comparePasswords(event) {
     const password = document.getElementById('user_password').value;
@@ -13,5 +13,14 @@ function comparePasswords(event) {
         document.getElementById('user_confirmpassword').value = '';
         document.getElementById('user_confirmpassword').focus();
         document.getElementById('user_confirmpassword').removeAttribute('placeholder');
+    }
+}
+
+function checkMissingNames() {
+    if (!document.getElementById('user_firstname').checkValidity()) {
+        document.getElementById('user_firstname').removeAttribute('placeholder');
+    }
+    if (!document.getElementById('user_lastname').checkValidity()) {
+        document.getElementById('user_lastname').removeAttribute('placeholder');
     }
 }
