ba52389964eb0552de38184fb645c9993ba5db18
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-09-04 16:33:45 +0200

    Add visual aid in case of failed password confirmation

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const signUpForm = document.getElementById('signup-form');
 const submitButton = document.getElementById('signup-btn');
 
 
-submitButton.addEventListener('click', (e) => comparePasswords(e));
+signUpForm.addEventListener('submit', (e) => comparePasswords(e));
 
 function comparePasswords(event) {
     const password = document.getElementById('user_password').value;
     const confirmPassword = document.getElementById('user_confirmpassword').value;
-    console.log(password);
-    console.log(confirmPassword);
     if (password !== confirmPassword) {
         event.preventDefault();
         console.log('Passwords not matching');
+        document.getElementById('user_confirmpassword').value = '';
+        document.getElementById('user_confirmpassword').focus();
+        document.getElementById('user_confirmpassword').removeAttribute('placeholder');
     }
 }
